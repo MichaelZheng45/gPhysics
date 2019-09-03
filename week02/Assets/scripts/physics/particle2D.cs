6ff96894d010843c3ee61162d88159eabeee0dbc
mike <michael.zheng@mymail.champlain.edu>
2019-09-03 10:43:47 -0400

    new repo

---
+++ week02/Assets/scripts/physics/particle2D.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class particle2D : MonoBehaviour
+{
+    //step 1
+    public Vector2 position, velocity, acceleration;
+
+    //step 2
+    void updatePositionEulerExplicit(float dt)
+    {
+        //x(t + dt) = x(t) + v(t)dt
+        //Euler's Method
+        //F(t+dt) = F(t) + f(t)dt
+        //               + (dF/dt)dt
+        position += velocity * dt;
+
+        //**** more to do here ****
+        //v(t*dt) = v(t) + a(t)dt
+        velocity += acceleration * dt;
+    }
+    void updatePositionKinematic(float dt)
+    {
+
+    }
+
+    void updateRotationEulerExplicit(float dt)
+    {
+
+    }
+
+    void updateRotationKinematic(float dt)
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        //step 3
+        updatePositionEulerExplicit(Time.fixedDeltaTime);
+        transform.position = position;
+
+        //step 4
+        acceleration.x = -Mathf.Sin(Time.fixedTime);
+    }
+}
