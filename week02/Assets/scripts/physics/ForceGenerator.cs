9fd6977cb79d409ce65f60b18b14ec2cbc22557d
michael <michael.zheng@mymail.champlain.edu>
2019-09-13 09:57:27 -0400

    current work

--- week02/Assets/scripts/physics/ForceGenerator.cs
+++ week02/Assets/scripts/physics/ForceGenerator.cs
@@ -14,7 +14,8 @@ public class ForceGenerator
     public static Vector2 GenerateForce_normal(Vector2 f_gravity, Vector2 surfaceNormal_unit)
     {
         // f_normal = proj(f_gravity, surfaceNormal_unit)
-        Vector2 f_normal = 
+        Vector2 f_normal = f_gravity.magnitude * surfaceNormal_unit;
+        return f_normal;
     }
 
     public static Vector2 GenerateForce_sliding(Vector2 f_gravity, Vector2 f_normal)
@@ -34,11 +35,14 @@ public class ForceGenerator
     public static Vector2 GenerateForce_friction_kinetic(Vector2 f_normal, Vector2 particleVelocity, float frictionCoefficient_kinetic)
     {
         // f_friction_k = -coeff*|f_normal| * unit(vel)
+        Vector2 f_friction_k = -frictionCoefficient_kinetic * new Vector2(Mathf.Abs(f_normal.x),Mathf.Abs(f_normal.y)) * particleVelocity.magnitude;
+        return f_friction_k;
     }
 
     public static Vector2 GenerateForce_drag(Vector2 particleVelocity, Vector2 fluidVelocity, float fluidDensity, float objectArea_crossSection, float objectDragCoefficient)
     {
-        // f_spring = -coeff*(spring length - spring resting length)
+        // f_drag = (p * u^2 * area * coeff)/2, p = density,  
+        Vector2 f_drag = ()
     }
 
     public static Vector2 GenerateForce_spring(Vector2 particlePosition, Vector2 anchorPosition, float springRestingLength, float springStiffnessCoefficient)
