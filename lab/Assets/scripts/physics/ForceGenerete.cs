79de1934a59683d0a8822e20c98aef72df82af02
michaelzheng <michael.zheng@mymail.champlain.edu>
2019-09-20 10:12:14 -0400

    added dan's fixes

--- lab/Assets/scripts/physics/ForceGenerete.cs
+++ lab/Assets/scripts/physics/ForceGenerete.cs
@@ -28,14 +28,15 @@ public class ForceGenerator
     public static Vector2 GenerateForce_friction_static(Vector2 f_normal, Vector2 f_opposing, float frictionCoefficient_static)
     {
         // f_friction_s = -f_opposing if less than max, else -coeff*f_normal (max amount is coeff*|f_normal|)
-        Vector2 f_friction_s = -f_opposing.magnitude < (frictionCoefficient_static * f_normal.magnitude) ? -f_opposing : -frictionCoefficient_static * f_normal;
-        if (f_opposing.magnitude < (frictionCoefficient_static * f_normal).magnitude)
+        Vector2 f_friction_s;
+        float max = frictionCoefficient_static * f_normal.magnitude;
+        if (f_opposing.magnitude < max)
         {
             f_friction_s = -f_opposing;
         }
         else
         {
-            f_friction_s = -frictionCoefficient_static * f_normal.magnitude * f_opposing.normalized; //direction is probably -f_oposing
+            f_friction_s = -max * f_opposing.normalized; //direction is probably -f_oposing
         }
         return f_friction_s;
     }
@@ -50,16 +51,17 @@ public class ForceGenerator
     public static Vector2 GenerateForce_drag(Vector2 particleVelocity, Vector2 fluidVelocity, float fluidDensity, float objectArea_crossSection, float objectDragCoefficient)
     {
         // f_drag = (p * u^2 * area * coeff)/2, p = density, u= velocity  https://www.grc.nasa.gov/WWW/k-12/airplane/drageq.html
-        float relativeVelocity = (particleVelocity - fluidVelocity).magnitude;
-        Vector2 f_drag = -particleVelocity.normalized * (fluidDensity * (relativeVelocity * relativeVelocity) * objectArea_crossSection * objectDragCoefficient) / 2;
+        Vector2 relativeVelocity = (particleVelocity - fluidVelocity);
+        float velocityScale = relativeVelocity.magnitude;
+        Vector2 f_drag = -relativeVelocity.normalized* (fluidDensity * (velocityScale * velocityScale) * objectArea_crossSection * objectDragCoefficient) * .5f;
         return f_drag;
     }
 
     public static Vector2 GenerateForce_spring(Vector2 particlePosition, Vector2 anchorPosition, float springRestingLength, float springStiffnessCoefficient)
     {
         // f_spring = -coeff*(spring length - spring resting length)
-        Vector2 direction = (particlePosition - anchorPosition).normalized;
-        Vector2 f_spring = -springStiffnessCoefficient * ((particlePosition - anchorPosition).magnitude - springRestingLength) * direction;
+        Vector2 direction = (particlePosition - anchorPosition);
+        Vector2 f_spring = -springStiffnessCoefficient * (direction.magnitude - springRestingLength) * direction.normalized;
         return f_spring;
     }
 }
