2ae237778b6bd37e382ce00e5cac06fd592a8b0a
mz <michael.zheng@mymail.champlain.edu>
2019-10-22 10:48:19 -0400

    initial work and moving scripts

---
+++ lab/Assets/scripts/physics/Quaternion4D.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quaternion4D 
+{
+    float w; 
+    float x;
+    float y;
+    float z;
+
+    public void setQuaternion(float rotation, Vector3 axis)
+    {
+        w = Mathf.Cos(rotation * 0.5f);
+        x = Mathf.Sin(rotation * 0.5f);
+        y = Mathf.Sin(rotation * 0.5f);
+        z = Mathf.Sin(rotation * 0.5f);
+    }
+}
---
+++ lab/Assets/scripts/physics/particle3D.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class particle3D : MonoBehaviour
+{
+    public particleType typeOfParticle;
+    public Vector3 position, posVelocity, posAcceleration;
+    public Vector3 rotation, rotVelocity, rotAcceleration;
+
+    [SerializeField]
+    rotationUpdate rotationMode = rotationUpdate.ROTATION_KINEMATIC;
+    [SerializeField]
+    positionUpdate positionMode = positionUpdate.POSITION_KINEMATIC;
+    [SerializeField]
+    InertiaTypes i_mode = InertiaTypes.CIRCLE;
+
+    [Range(0f, 1f)]
+    public float elasticity;
+
+    //lab 2 step 1
+    public float startingMass;
+    float mass, massInv;
+
+    float coeff_static = .62f;
+    float coeffc_kinetic = .48f;
+
+    //Position Force
+    Vector3 force;
+
+    //Center of mass
+    public Vector3 centOfMass = new Vector3(0.5f, 0.5f,0.5f);
+
+    //Rotational Force
+    float inertia, inertiaInv;
+    Vector3 torque;
+
+    //force bool activates
+    public bool isGravity = false;
+    public bool isDrag = false;
+
+    public float getInertia()
+    {
+        return inertia;
+    }
+
+    public void setInertia(float newInertia)
+    {
+        inertia = newInertia;
+        inertiaInv = inertia > 0.0f ? 1.0f / inertia : 0;
+    }
+
+    public void setMass(float newMass)
+    {
+        mass = newMass > 0.0f ? newMass : 0.0f;
+        //mass = mathf.max(0,newMass);
+
+        massInv = mass > 0.0f ? 1.0f / mass : 0;
+    }
+
+    public float getMass()
+    {
+        return mass;
+    }
+
+    public float getInverseMass()
+    {
+        return 1f / mass;
+    }
+
+    //force application lab 2 step 2
+    public void AddForce(Vector3 newForce)
+    {
+        //D'Alembert
+        force += newForce;
+    }
+
+    public void AddTorque(Vector3 newTorque)
+    {
+        torque += newTorque;
+    }
+
+    void updateAcceleration()
+    {
+        //newtons 2nd law
+        posAcceleration = force * massInv;
+        force.Set(0.0f, 0.0f, 0.0f);
+
+        //torque
+        rotAcceleration = torque * inertiaInv;
+        torque = Vector3.zero; ;
+    }
+
+    //step 2
+    void updatePositionEulerExplicit(float dt)
+    {
+        position += posVelocity * dt;
+        posVelocity += posAcceleration * dt;
+    }
+    void updatePositionKinematic(float dt)
+    {
+        position += posVelocity * dt + (1 / 2) * posAcceleration * (dt * dt);
+        posVelocity += posAcceleration * dt;
+    }
+
+    void updateRotationEulerExplicit(float dt)
+    {
+        rotation += rotVelocity * dt;
+
+        rotVelocity += rotAcceleration * dt;
+    }
+
+    void updateRotationKinematic(float dt)
+    {
+        rotation += rotVelocity * dt + (1 / 2) * rotAcceleration * (dt * dt);
+        rotVelocity += (rotAcceleration * dt);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        setMass(startingMass);
+
+        switch (i_mode)
+        {
+            case InertiaTypes.RECTANGE:
+                inertia = InertiaGenerator.GenerateInertia_Rectangle(mass, centOfMass.x * 2, centOfMass.y * 2);
+                break;
+
+            case InertiaTypes.CIRCLE:
+                inertia = InertiaGenerator.GenerateInertia_Circle(mass, centOfMass.x);
+                break;
+
+            case InertiaTypes.RING:
+                inertia = InertiaGenerator.GenerateInertia_Ring(mass, centOfMass.x, 0.4f);
+                break;
+
+            case InertiaTypes.ROD:
+                inertia = InertiaGenerator.GenerateInertia_Rectangle(mass, centOfMass.x, centOfMass.y);
+                break;
+        }
+        setInertia(inertia);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float dt = Time.fixedDeltaTime;
+
+        switch (rotationMode)
+        {
+            case rotationUpdate.ROTATION_EULER_EXPLICIT:
+                updateRotationEulerExplicit(dt);
+                break;
+            case rotationUpdate.ROTATION_KINEMATIC:
+                updateRotationKinematic(dt);
+                break;
+        }
+        transform.rotation = Quaternion.Euler(rotation);
+
+        switch (positionMode)
+        {
+            case positionUpdate.POSITION_EULER_EXPLICIT:
+                updatePositionEulerExplicit(dt);
+                break;
+            case positionUpdate.POSITION_KINEMATIC:
+                updatePositionKinematic(dt);
+                break;
+        }
+        transform.position = position;
+
+        //accelerationUpdate
+        updateAcceleration();
+    }
+
+    public void setBase(Vector3 nPosition, Vector3 nRotation)
+    {
+        rotation = nRotation;
+        position = nPosition;
+    }
+}
