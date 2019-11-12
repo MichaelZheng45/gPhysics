a842ffc938c0b24565c1467662973295d7be771d
mz <michael.zheng@mymail.champlain.edu>
2019-11-12 16:45:53 -0500

    added plugin

---
+++ lab/Assets/scripts/Plugins/MyUnityPlugin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine;
+
+using System.Runtime.InteropServices;
+
+public class MyUnityPLugin
+{
+    [DllImport("MyUnityPlugin")]
+    public static extern int InitFoo(int f_new = 0);
+
+    [DllImport("MyUnityPlugin")]
+    public static extern int DoFoo(int bar = 0);
+
+    [DllImport("MyUnityPlugin")]
+    public static extern int TermFoo();
+}
